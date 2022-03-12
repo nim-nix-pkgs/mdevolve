1e1249fc32df440d3598e08d38b6f68da4f8c0d0
hugosenari <hugosenari@gmail.com>
2022-03-12 03:21:48 -0300

    chore: re index mdevolve

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = ''Integrator framework for Molecular Dynamic evolutions'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."mdevolve-master".url = "path:./master";
+  inputs."mdevolve-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."mdevolve-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."mdevolve-v0_1_1".url = "path:./v0_1_1";
+  inputs."mdevolve-v0_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."mdevolve-v0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."mdevolve-v1_0_0".url = "path:./v1_0_0";
+  inputs."mdevolve-v1_0_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."mdevolve-v1_0_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Integrator framework for Molecular Dynamic evolutions'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-mdevolve-master.flake = false;
+  inputs.src-mdevolve-master.owner = "jxy";
+  inputs.src-mdevolve-master.ref   = "refs/heads/master";
+  inputs.src-mdevolve-master.repo  = "MDevolve";
+  inputs.src-mdevolve-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-mdevolve-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-mdevolve-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
